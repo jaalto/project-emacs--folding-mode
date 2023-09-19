a6f9989bfbe7c91302adc6655e24caadafdfcd7b
Jari Aalto <jari.aalto@cante.net>
2023-09-19 13:45:36 +0300

    folding.el: (folding-mode): docstring width 80

--- folding.el
+++ folding.el
@@ -18,7 +18,7 @@
 ;; [Latest devel version]
 ;; Vcs-URL:     https://github.com/jaalto/project-emacs--folding-mode
 
-(defconst folding-version-time "2023.0919.1043"
+(defconst folding-version-time "2023.0919.1045"
   "Last edit time in format YYYY.MMDD.HHMM.")
 
 ;;{{{ GPL
@@ -3170,116 +3170,132 @@ Keys starting with `folding-mode-prefix-key'
 
      folding-convert-buffer-for-printing:
      `\\[folding-convert-buffer-for-printing]'
-     Makes a ready-to-print, formatted, unfolded copy in another buffer.
+     Makes a ready-to-print, formatted, unfolded copy in another
+     buffer.
 
-     Read the documentation for the above functions for more information.
+     Read the documentation for the above functions for more
+     information.
 
 Overview
 
-    Folds are a way of hierarchically organizing the text in a file, so
-    that the text can be viewed and edited at different levels. It is
-    similar to Outline mode in that parts of the text can be hidden from
-    view. A fold is a region of text, surrounded by special \"fold marks\",
-    which act like brackets, grouping the text. Fold mark pairs can be
-    nested, and they can have titles. When a fold is folded, the text is
-    hidden from view, except for the first line, which acts like a title
-    for the fold.
+    Folds are a way of hierarchically organizing the text in a
+    file, so that the text can be viewed and edited at different
+    levels. It is similar to Outline mode in that parts of the
+    text can be hidden from view. A fold is a region of text,
+    surrounded by special \"fold marks\", which act like
+    brackets, grouping the text. Fold mark pairs can be nested,
+    and they can have titles. When a fold is folded, the text is
+    hidden from view, except for the first line, which acts like
+    a title for the fold.
 
-    Folding mode is a minor mode, designed to cooperate with many other
-    major modes, so that many types of text can be folded while they are
-    being edited (eg., plain text, program source code, Texinfo, etc.).
+    Folding mode is a minor mode, designed to cooperate with many
+    other major modes, so that many types of text can be folded
+    while they are being edited (eg., plain text, program source
+    code, Texinfo, etc.).
 
 Folding-mode function
 
-    If Folding mode is not called interactively (`(folding-called-interactively-p)' is nil),
-    and it is called with two or less arguments, all of which are nil, then
-    the point will not be altered if `folding-folding-on-startup' is set
-    and `folding-whole-buffer' is called. This is generally not a good
-    thing, as it can leave the point inside a hidden region of a fold, but
-    it is required if the local variables set \"mode: folding\" when the
-    file is first read (see `hack-local-variables').
+    If Folding mode is not called
+    interactively (`(folding-called-interactively-p)' is nil),
+    and it is called with two or less arguments, all of which are
+    nil, then the point will not be altered if
+    `folding-folding-on-startup' is set and
+    `folding-whole-buffer' is called. This is generally not a
+    good thing, as it can leave the point inside a hidden region
+    of a fold, but it is required if the local variables set
+    \"mode: folding\" when the file is first read (see
+    `hack-local-variables').
 
-    Not that you should ever want to, but to call Folding mode from a
-    program with the default behavior (toggling the mode), call it with
-    something like `(folding-mode nil t)'.
+    Not that you should ever want to, but to call Folding mode
+    from a program with the default behavior (toggling the mode),
+    call it with something like `(folding-mode nil t)'.
 
 Fold marks
 
-    For most types of folded file, lines representing folds have \"{{{\"
-    near the beginning. To enter a fold, move the point to the folded line
-    and type `\\[folding-shift-in]'. You should no longer be able to see
-    the rest of the file, just the contents of the fold, which you couldn't
-    see before. You can use `\\[folding-shift-out]' to leave a fold, and
-    you can enter and exit folds to move around the structure of the file.
+    For most types of folded file, lines representing folds have
+    \"{{{\" near the beginning. To enter a fold, move the point
+    to the folded line and type `\\[folding-shift-in]'. You
+    should no longer be able to see the rest of the file, just
+    the contents of the fold, which you couldn't see before. You
+    can use `\\[folding-shift-out]' to leave a fold, and you can
+    enter and exit folds to move around the structure of the
+    file.
 
-    All of the text is present in a folded file all of the time. It is just
-    hidden. Folded text shows up as a line (the top fold mark) with \"...\"
-    at the end. If you are in a fold, the mode line displays \"inside n
-    folds Narrow\", and because the buffer is narrowed you can't see outside
-    of the current fold's text.
+    All of the text is present in a folded file all of the time.
+    It is just hidden. Folded text shows up as a line (the top
+    fold mark) with \"...\" at the end. If you are in a fold, the
+    mode line displays \"inside n folds Narrow\", and because the
+    buffer is narrowed you can't see outside of the current
+    fold's text.
 
-    By arranging sections of a large file in folds, and maybe subsections
-    in sub-folds, you can move around a file quickly and easily, and only
-    have to scroll through a couple of pages at a time. If you pick the
-    titles for the folds carefully, they can be a useful form of
-    documentation, and make moving though the file a lot easier. In
-    general, searching through a folded file for a particular item is much
-    easier than without folds.
+    By arranging sections of a large file in folds, and maybe
+    subsections in sub-folds, you can move around a file quickly
+    and easily, and only have to scroll through a couple of pages
+    at a time. If you pick the titles for the folds carefully,
+    they can be a useful form of documentation, and make moving
+    though the file a lot easier. In general, searching through a
+    folded file for a particular item is much easier than without
+    folds.
 
 Managing folds
 
-    To make a new fold, set the mark at one end of the text you want in the
-    new fold, and move the point to the other end. Then type
-    `\\[folding-fold-region]'. The text you selected will be made into a
-    fold, and the fold will be entered. If you just want a new, empty fold,
-    set the mark where you want the fold, and then create a new fold there
-    without moving the point. Don't worry if the point is in the middle of
-    a line of text, `folding-fold-region' will not break text in the middle
-    of a line. After making a fold, the fold is entered and the point is
-    positioned ready to enter a title for the fold. Do not delete the fold
-    marks, which are usually something like \"{{{\" and \"}}}\". There may
-    also be a bit of fold mark which goes after the fold title.
+    To make a new fold, set the mark at one end of the text you
+    want in the new fold, and move the point to the other end.
+    Then type `\\[folding-fold-region]'. The text you selected
+    will be made into a fold, and the fold will be entered. If
+    you just want a new, empty fold, set the mark where you want
+    the fold, and then create a new fold there without moving the
+    point. Don't worry if the point is in the middle of a line of
+    text, `folding-fold-region' will not break text in the middle
+    of a line. After making a fold, the fold is entered and the
+    point is positioned ready to enter a title for the fold. Do
+    not delete the fold marks, which are usually something like
+    \"{{{\" and \"}}}\". There may also be a bit of fold mark
+    which goes after the fold title.
 
-    If the fold markers get messed up, or you just want to see the whole
-    unfolded file, use `\\[folding-open-buffer]' to unfolded the whole
-    file, so you can see all the text and all the marks. This is useful for
-    checking/correcting unbalanced fold markers, and for searching for
-    things. Use `\\[folding-whole-file]' to fold the buffer again.
+    If the fold markers get messed up, or you just want to see
+    the whole unfolded file, use `\\[folding-open-buffer]' to
+    unfolded the whole file, so you can see all the text and all
+    the marks. This is useful for checking/correcting unbalanced
+    fold markers, and for searching for things. Use
+    `\\[folding-whole-file]' to fold the buffer again.
 
-    `folding-shift-out' will attempt to tidy the current fold just before
-    exiting it. It will remove any extra blank lines at the top and bottom,
-    \(outside the fold marks). It will then ensure that fold marks exists,
-    and if they are not, will add them (after asking). Finally, the number
-    of blank lines between the fold marks and the contents of the fold is
-    set to 1 (by default).
+    `folding-shift-out' will attempt to tidy the current fold
+    just before exiting it. It will remove any extra blank lines
+    at the top and bottom, \(outside the fold marks). It will
+    then ensure that fold marks exists, and if they are not, will
+    add them (after asking). Finally, the number of blank lines
+    between the fold marks and the contents of the fold is set to
+    1 (by default).
 
 Folding package customizations
 
-    If the fold marks are not set on entry to Folding mode, they are set to
-    a default for current major mode, as defined by
-    `folding-mode-marks-alist' or to \"{{{ \" and \"}}}\" if none are
-    specified.
+    If the fold marks are not set on entry to Folding mode, they
+    are set to a default for current major mode, as defined by
+    `folding-mode-marks-alist' or to \"{{{ \" and \"}}}\" if none
+    are specified.
 
-    To bind different commands to keys in Folding mode, set the bindings in
-    the keymap `folding-mode-map'.
+    To bind different commands to keys in Folding mode, set the
+    bindings in the keymap `folding-mode-map'.
 
-    The hooks `folding-mode-hook' and `<major-mode-name>-folding-hook' are
-    called before folding the buffer and applying the key bindings in
-    `folding-mode-map'. This is a good hook to set extra or different key
-    bindings in `folding-mode-map'. Note that key bindings in
-    `folding-mode-map' are only examined just after calling these hooks;
-    new bindings in those maps only take effect when Folding mode is being
-    started. The hook `folding-load-hook' is called when Folding mode is
-    loaded into Emacs.
+    The hooks `folding-mode-hook' and
+    `<major-mode-name>-folding-hook' are called before folding
+    the buffer and applying the key bindings in
+    `folding-mode-map'. This is a good hook to set extra or
+    different key bindings in `folding-mode-map'. Note that key
+    bindings in `folding-mode-map' are only examined just after
+    calling these hooks; new bindings in those maps only take
+    effect when Folding mode is being started. The hook
+    `folding-load-hook' is called when Folding mode is loaded
+    into Emacs.
 
 Mouse behavior
 
-    If you want folding to detect point of actual mouse click, please see
-    variable `folding-mouse-yank-at-p'.
+    If you want folding to detect point of actual mouse click,
+    please see variable `folding-mouse-yank-at-p'.
 
     To customise the mouse actions, look at `folding-behave-table'."
   (interactive)
-
   (let ((new-folding-mode
          (if (not arg)
              (not folding-mode)
